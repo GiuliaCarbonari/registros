508802a5477e74169fb256fff2a8888d7e55081b
giulicarbonari <50453356+giulicarbonari@users.noreply.github.com>
2020-05-26 20:39:10 -0300

    Cambio orden de canales visibles en plot

--- anotaciones.py
+++ anotaciones.py
@@ -112,39 +112,21 @@ def main():
         raw = set_sleep_states(raw,path_states)
 
     # -----------------------------------------------------------------
-    data =raw.get_data()         # data(chan,samp), times(1xsamples)
-    info = raw.info              #info
-    sfreq = info.get('sfreq')    #frecuencia de muestreo
 
-    #Con este código extraigo los datos que necesito y me rearmo la estructura que necesito para poder analizarlo mejor
-    data =raw.get_data()                                 # Saco los datos concretos, una matriz de numpy
-    new_data=data.copy()
-    canal_eogs = data[6,:] - data[7,:]                   # Cree la variable de la resta de las dos señales
-    canal_emgs =  data[8,:] - data[9,:]
-    t = np.linspace(1, round(data.shape[1]/sfreq), data.shape[1], endpoint=False)   
-    new_data[0]= signal.square(2 * np.pi * 1 * t)
-    new_data[1]=data[[0], :]
-    new_data[2]=data[[1], :]
-    new_data[3]=canal_eogs
-    new_data[4]=canal_emgs
+    info = raw.info
+    sfreq = info.get('sfreq') #frecuencia de muestreo
+    raw = new_raw_data(raw,sfreq)
+  
+    subject = get_name(path)
+    info = raw.info
+    sfreq = info.get('sfreq') #frecuencia de muestre
+    
+    raw.plot(show_options=True,title='Etiquetado',start=0,duration=30,n_channels=10, scalings=scal,block=True,order=[0,3,2,4,1])
+    #order cambia el orden en el que aparecen los canales
 
-    new_data=new_data[[0,1,2,3,4], :]        #Elimino los otros canales
+    raw.annotations.save(subject + "Annotations.txt")
 
-    new_chnames =[ "Pulso", raw.ch_names[0], raw.ch_names[1], "EOG_resta", "EMG_resta"] 
-    new_chtypes = ['misc'] +['eeg' for _ in new_chnames[0:2]] + ['misc','misc'] # Recompongo los canales.
-
-    new_info = mne.create_info(new_chnames, sfreq, ch_types=new_chtypes)
-    new_info['meas_date'] = raw.info['meas_date']       # Registro el timestamp para las anotaciones.
-
-    new_raw=mne.io.RawArray(new_data, new_info)
-    new_raw.set_annotations(raw.annotations)           # Construyo un nuevo objeto raw que tiene lo que necesito.
-
-    scal = dict(mag=1e-12, grad=4e-11, eeg=20e-5, eog=150e-6, ecg=5e-4,emg=1e-4, ref_meg=1e-12, misc=1e-3, stim=1,
-        resp=1, chpi=1e-4, whitened=1e2)
-
-    #pplot=new_raw.plot(scalings=scal, duration=30, n_channels=10, block=True)
-    new_raw.plot(show_options=True,title='Etiquetado',start=0,duration=30,n_channels=10, scalings=scal,block=True)
-    new_raw.annotations.save(subject + "Annotations.txt")
+    raw.save(subject + ".fif")
 
 
 
